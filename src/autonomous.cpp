ce57982ecb0d2ad06b9dd2df41ab5ab1275663d8
James Tan <jameshuatan@gmail.com>
2020-02-25 21:08:17 +0000

    Remove auton from skills temporarily

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -15,31 +15,4 @@
  * re-start it from where it left off.
  */
 void autonomous() {
-  deployRobot(true);
-  intakeFiveCubes();
-  setIntake(0.75);
-  chassisControl->turnAngle(-45_deg, chassisControl->rightPivot);
-  robotModel->setMaxVoltage(12000);
-  chassisControl->moveDistance(-0.5_ft);
-  chassisControl->turnAngle(-137_deg);
-  chassisControl->moveDistance(1.5_ft);
-  chassisControl->turnAngle(-47_deg);
-  setIntake(0);
-  robotModel->forward(0.8);
-  setIntake(-0.5);
-  pros::delay(200);
-  setIntake(0);
-  pros::delay(800);
-  robotModel->stop();
-  chassisControl->moveDistance(-0.15_ft);
-  tilterMtr.moveVoltage(12000);
-  while (tilterMtr.getPosition() < 7500) {
-    pros::delay(10);
-  }
-  tilterMtr.moveVoltage(12000);
-  while (tilterMtr.getPosition() < 9500) {
-    pros::delay(10);
-  }
-  tilterMtr.moveVoltage(0);
-  chassisControl->moveDistance(-1_ft);
 }
